bda62c976397414be026822d290ab6f37e49ae16
Atomiqu <atomiqu@quantumly.dev>
2021-10-24 13:09:53 +0200

    chore: upgrade deps

--- rollup.config.ts
+++ rollup.config.ts
@@ -42,8 +42,7 @@ export default {
 			ecma: 2019,
 			// This will ensure that whenever Rollup is in watch (dev) mode, console logs will not be removed
 			// eslint-disable-next-line @typescript-eslint/naming-convention
-			compress: { drop_console: !Reflect.has(process.env, 'ROLLUP_WATCH') },
-			output: { comments: false }
+			compress: { drop_console: !Reflect.has(process.env, 'ROLLUP_WATCH') }
 		})
 	]
 };
